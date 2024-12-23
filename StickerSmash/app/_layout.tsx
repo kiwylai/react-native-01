a7bf6d85df7b863149c6a0d3875e1143bac795a7
Kiwy <kiwylai@gmial.com>
2024-12-23 16:00:10 +0100

    add notFound and about pages

--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: "Sticker Smash",
+          headerLeft: () => <></>,
+        }}
+      />
+      <Stack.Screen name="about" options={{ headerTitle: "About" }} />
+      <Stack.Screen name="notFound" options={{ headerTitle: "Not found" }} />
+    </Stack>
+  );
 }
