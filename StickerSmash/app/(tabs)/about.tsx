f7908bc1f2ea1dc3cd140009a61f7298ae1b72bc
kiwylai <kiwylai@gmail.com>
2024-12-31 17:26:29 +0100

    add describe in about page

--- StickerSmash/app/(tabs)/about.tsx
+++ StickerSmash/app/(tabs)/about.tsx
@@ -3,7 +3,17 @@ import { Text, View, StyleSheet } from "react-native";
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen.</Text>
+      <Text style={styles.title}>About This App</Text>
+      <Text style={styles.paragraph}>
+        This app is designed as a simple practice project to demonstrate the
+        basics of React Native. Explore, experiment, and learn how to create
+        amazing mobile applications with ease!
+      </Text>
+
+      <Text style={styles.paragraph}>
+        The app allows you to add pictures and emojis, you can also take a
+        screenshot and save it in your phone/computer. Enjoy your journey!
+      </Text>
     </View>
   );
 }
@@ -11,11 +21,19 @@ export default function AboutScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    padding: 20,
     backgroundColor: "#25292e",
   },
-  text: {
-    color: "white",
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#fff",
+  },
+  paragraph: {
+    fontSize: 16,
+    lineHeight: 24,
+    marginBottom: 15,
+    color: "#fff",
   },
 });
